043c4fd73088c2586d93d584355ad1523cd96d26
Anslie Brant <brantae@gmail.com>
2023-02-20 19:35:08 -0600

    commit tree 643b4028983227fd46c42c47348da27c1fe9c1ad

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+# x = 1
+# while x < 10
+#     puts "#{x} is less than 10"
+#     x += 1
+# end
+
+def countdown(int)
+    while int > 0
+        puts "#{int} SECOND(S)!"
+        int -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+countdown(10)
